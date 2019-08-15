1f6a793bf4a696165d661bb66d399018d786a36d
iamrenejr <calunsag.renejr@gmail.com>
2019-08-15 16:21:55 +0800

    Updating dependency of core to 0.1.10 and adjusting accordingly

--- src/aegis.js
+++ src/aegis.js
@@ -1,22 +1,20 @@
 const uniqid = require("uniqid");
-const { compose } = require("ramda");
-const { BehaviorSubject } = require("rxjs");
-const { taskToObservable } = require("@ewise/aegisjs-core/frpcore/transforms");
 const { requestToAegisOTA, requestToAegisServerWithToken } = require("@ewise/aegisjs-core/hof/requestToAegis");
-const { kickstart$: initPollingStream } = require("@ewise/aegisjs-core/hos/pollingCore");
+const { addDelay } = require("@ewise/aegisjs-core/frpcore/pointfree");
 const { safeMakeWebUrl } = require("@ewise/aegisjs-core/fpcore/safeOps");
+const {
+    DEFAULT_POLLING_INTERVAL,
+    DEFAULT_REQUEST_TIMEOUT,
+    DEFAULT_RETY_LIMIT,
+    DEFAULT_DELAY_BEFORE_RETRY,
+    DEFAULT_AGGREGATE_WITH_TRANSACTIONS
+} = require("@ewise/aegisjs-core/constants/standardValues");
+const HTTP_VERBS = require("@ewise/aegisjs-core/constants/httpVerbs");
+const {
+    createRecursivePollStream
+} = require("@ewise/aegisjs-core");
 
-const DEFAULT_REQUEST_TIMEOUT = 90000; //ms
-const DEFAULT_RETY_LIMIT = 5;
-const DEFAULT_DELAY_BEFORE_RETRY = 5000; //ms
-const DEFAULT_AGGREGATE_WITH_TRANSACTIONS = true;
-
-const HTTP_VERBS = {
-    GET: "GET",
-    POST: "POST",
-    PUT: "PUT",
-    DELETE: "DELETE"
-};
+const createRecursivePDVPollStream = createRecursivePollStream("error", "partial", "stopped", "done");
 
 const PDV_PATHS = otaUrl => {
     const writeUrl = path => otaUrl ? safeMakeWebUrl(path, otaUrl).getOrElse("") : path;
@@ -95,44 +93,75 @@ const aegis = (options = {}) => {
                 timeout = defaultTimeout,
                 retryLimit = defaultRetryLimit,
                 retryDelay = defaultRetryDelay,
+                pollingInterval: pollInterval = DEFAULT_POLLING_INTERVAL,
                 withTransactions: transactions = DEFAULT_AGGREGATE_WITH_TRANSACTIONS,
                 ajaxTaskFn = defaultAjaxTaskFn
             } = args;
 
-            const TERMINAL_PDV_STATES = ["error", "partial", "stopped", "done"];
-            const stopStreamCondition = arg => arg ? TERMINAL_PDV_STATES.indexOf(arg.status) === -1 : false;
-
             const xheaders = { appId, appSecret, uname, email };
             const paths = PDV_PATHS(otaUrl);
-
-            const subject$ = new BehaviorSubject({ processId: null });
 
             const csrf = uniqid();
             const bodyCsrf = { code: instCode, prompts, challenge: csrf, transactions };
 
-            const startAegisOTA = () => ajaxTaskFn(
-                HTTP_VERBS.POST,
-                jwt,
-                xheaders,
-                bodyCsrf,
-                timeout,
-                PDV_PATHS(otaUrl).START_OTA
-            );
+/*
+            const TERMINAL_PDV_STATES = ["error", "partial", "stopped", "done"];
+            const stopStreamCondition = arg => arg ? TERMINAL_PDV_STATES.indexOf(arg.status) === -1 : false;
 
-            const checkAegisOTA = pid => ajaxTaskFn(
-                HTTP_VERBS.GET,
-                jwt,
-                xheaders,
-                null,
-                timeout,
-                PDV_PATHS(otaUrl).QUERY_OTA(pid, csrf)
-            );
+            const subject$ = new BehaviorSubject({ processId: null });
+
+            const startAegisOTA = ;
+
+            const checkAegisOTA = ;
 
             const initialStream$ = compose(taskToObservable, startAegisOTA);
             const pollingStream$ = compose(taskToObservable, checkAegisOTA);
             const createStream = initPollingStream(retryLimit, retryDelay);
             const stream$ = createStream(stopStreamCondition, initialStream$, pollingStream$);
+*/
 
+            return createRecursivePDVPollStream({
+                retryLimit,
+                retryDelay,
+                start: () => ajaxTaskFn(
+                    HTTP_VERBS.POST,
+                    jwt,
+                    xheaders,
+                    bodyCsrf,
+                    timeout,
+                    PDV_PATHS(otaUrl).START_OTA
+                ),
+                afterCheck: addDelay(pollInterval),
+                check: pid => ajaxTaskFn(
+                    HTTP_VERBS.GET,
+                    jwt,
+                    xheaders,
+                    null,
+                    timeout,
+                    PDV_PATHS(otaUrl).QUERY_OTA(pid, csrf)
+                ),
+                resume: (getPid, otp) =>
+                    ajaxTaskFn(
+                        HTTP_VERBS.POST,
+                        jwt,
+                        xheaders,
+                        { ...otp, challenge: csrf },
+                        timeout,
+                        paths.RESUME_OTA(getPid())
+                    ),
+                stop: pid =>
+                    ajaxTaskFn(
+                        HTTP_VERBS.DELETE,
+                        jwt,
+                        xheaders,
+                        null,
+                        timeout,
+                        paths.STOP_OTA(pid, csrf)
+                    )
+            
+            });
+
+/*
             return {
                 run: () =>
                     stream$.subscribe(
@@ -140,25 +169,8 @@ const aegis = (options = {}) => {
                         err => subject$.error(err),
                         () => subject$.complete(subject$.getValue())
                     ) && subject$,
-                resume: otp =>
-                    ajaxTaskFn(
-                        HTTP_VERBS.POST,
-                        jwt,
-                        xheaders,
-                        { ...otp, challenge: csrf },
-                        timeout,
-                        paths.RESUME_OTA(subject$.getValue().processId)
-                    ),
-                stop: () =>
-                    ajaxTaskFn(
-                        HTTP_VERBS.DELETE,
-                        jwt,
-                        xheaders,
-                        null,
-                        timeout,
-                        paths.STOP_OTA(subject$.getValue().processId, csrf)
-                    )
-            };
+                };
+*/
         }
     };
 };
